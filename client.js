3f32602bba1338dae89e505023315546bbb80eae
aliabdullah <ali.abdallah@bintricks.com>
2014-08-26 18:29:44 +0300

    connect, disconnect user in server/client

--- client.js
+++ client.js
@@ -1,6 +1,36 @@
-var io = require("socket.io-client")
+var io = require('socket.io-client')
+var readline = require('readline');
 var url     = "ws://localhost:3000";
 var options = { transports: ['websocket'] };
-var client  = io(url, options);
 
-var name = "";
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+var argumentLength = process.argv.length;
+if(argumentLength < 3 || process.argv.slice(2).toString()[0] != '-') {
+	console.log("node client.js -username");
+	console.log("ex: node client.js -aliabdallah");
+} else {
+	var client  = io(url, options);
+	var name = process.argv.slice(2).toString().substring(1);
+	client.on('connect', function() {
+		client.emit('connected', name);
+	});
+
+	client.on('getOnlineUser', function(onlineUsers) {
+		if(onlineUsers.length > 0) {
+			console.log("\nOnline Users\n");
+			for(var i = 0; i < onlineUsers.length; i++) {
+				console.log(onlineUsers[i]);
+			}
+			rl.question("Chat with: ", function(answer) {
+				console.log("Your Answer", answer);
+				rl.close();
+			})
+		} else {
+			console.log("No online users.");
+		}
+	});
+}
--- server.js
+++ server.js
@@ -1,22 +1,44 @@
 var io     = require("socket.io");
 var server = io();
 
-var onlineUsers = [];
+var onlineUsersIds = [];
+var onlineUsersNames = [];
 
+var printOnlineUsersNames = function() {
+	console.log("\n");
+	if(onlineUsersNames.length > 0) {
+		console.log("Online Users Now");
+		for(var i = 0; i< onlineUsersNames.length; i++) {
+			console.log(onlineUsersNames[i]);
+		}
+	} else {
+		console.log("No online users.");
+	}
+	console.log("\n");
+};
+console.log("server connected");
 server.on("connection", function (socket) {
 
-  	socket.on('connect', function(name) {
-  		socket.name = name;
-  		onlineUsers.push(socket);
+
+	socket.emit('getOnlineUser', onlineUsersNames);
+
+  	socket.on('connected', function(username) {
+
+  		onlineUsersNames.push(username);
+  		onlineUsersIds.push(socket.id);
+  		console.log("User " + username + " is online");
   	});
   	
   	socket.on('disconnect', function() {
-		clients.splice(clients.indexOf(socket), 1);
-		console.log("Online Users Now:");
-		for(var i = 0; i < clients.length; i++) {
-			console.log(onlineUsers[i].name);
-		}
+  		var index = onlineUsersIds.indexOf(socket.id);
+  		var id = socket.id;
+		console.log("User " + onlineUsersNames[index] + " is offline");
+		onlineUsersNames.splice(index, 1);
+		onlineUsersIds.splice(index, 1);
 	});
+
+  	//setTimeout(function() {}, 5000);
+
   /*
   var message = "Hello Idiot";
   socket.emit("message", message);
@@ -26,7 +48,5 @@ server.on("connection", function (socket) {
   });
 	*/
 });
-
-
 
 server.listen(3000);
